79846d1189af7c622a9a42b9d1046cccdefb1999
Dan Shechter <dans@houmus.org>
2023-11-29 18:55:23 +0200

    Initial version

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,50 @@
+//use egui::{Align2, CentralPanel, ComboBox, CtxRef, Frame, Label, Layout, TopBottomPanel, Ui, Vec2};
+//use eframe::{App, FrameContext, IntegrationInfo, setup_native, TextureAllocator};
+
+
+use egui::{CentralPanel, Label, TextStyle, TopBottomPanel};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct SpellingDojoApp {
     // Example stuff:
     label: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+
+    question_num: usize,
+    questions: Vec<String>,
+    answers: Vec<String>,
+    user_answers: Vec<String>,
+
+
 }
 
-impl Default for TemplateApp {
+impl Default for SpellingDojoApp {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+
+            question_num: 0,
+            user_answers: vec!["".to_string(), "".to_string()],
+
+            questions: vec![
+                "component".to_string(),
+                "spelling".to_string(),
+            ],
+            answers: vec![
+                "component".to_string(),
+                "spelling".to_string(),
+            ],
         }
     }
 }
 
-impl TemplateApp {
+impl SpellingDojoApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -33,64 +58,53 @@ impl TemplateApp {
 
         Default::default()
     }
+
+    fn check_answers(&self) -> Vec<bool> {
+        let mut results = Vec::new();
+        for (i, answer) in self.answers.iter().enumerate() {
+            results.push(answer == &self.questions[i]);
+        }
+        results
+    }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for SpellingDojoApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
+
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_dark_light_mode_buttons(ui);
-            });
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            ui.add(Label::new("Spelling Quiz"));
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
+                ui.add(Label::new(&self.questions[self.question_num]));
             });
         });
+
+        CentralPanel::default().show(ctx, |ui| {
+            ui.vertical(|ui| {
+                let answer = &self.answers[self.question_num];
+                for i in 0..answer.len() {
+                    ui.add(egui::TextEdit::singleline(&mut self.user_answers[i]));
+                }
+                
+                if ui.button("Submit Answer").clicked() {
+                    let errors = self.check_answers();
+                    let false_count = errors.iter().filter(|&b| *b == false).count();
+                    ui.add(Label::new(format!("{} false answers", false_count)));
+                    self.question_num = (self.question_num + 1) % self.questions.len();
+                    self.user_answers.clear();
+                    self.user_answers.resize(self.answers[self.question_num].len(), "".to_string());
+                }
+            });
+        });
+
+
+
     }
 }
 
